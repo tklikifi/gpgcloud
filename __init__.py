c609764d9b4f75dfea6b1b461773fcf026f1c664
Tommi Linnakangas <tommi.linnakangas@iki.fi>
2014-03-29 14:31:43 +0200

    Updated readme.

--- __init__.py
+++ __init__.py
@@ -1,22 +1,5 @@
 """
-System to store GPG encrypted data into cloud environments.
-
-GPGCloud is a system to store encrypted data into different cloud
-environments. The main focus of the tool is to use secure storage in the
-cloud: all data is encrypted and no information about what kind of data is
-stored is revealed to cloud provider.
-
-The security and privacy of the data is based on GPG encryption, which is
-performed outside the cloud environment.
-
-**YOU MUST KEEP YOUR GPG KEYRING SAFE, OTHERWISE YOUR DATA IS NOT SAFE
-EITHER.**
-
-Currently the following storage systems are supported:
-
-* Amazon S3
-* SFTP filesystems
-
+.. include:: ../README.md
 .. todo:: Add support for Google Cloud Storage.
 .. todo:: Add support for Dropbox.
 
