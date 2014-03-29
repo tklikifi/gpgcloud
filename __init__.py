cffa21d4fcfa07914c857e9d0c566b8feff2a691
Tommi Linnakangas <tommi.linnakangas@iki.fi>
2014-03-29 13:56:44 +0200

    Redraw the data flow chart.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 """
-Utility to store GPG encrypted data into cloud environments.
+System to store GPG encrypted data into cloud environments.
 
 GPGCloud is a system to store encrypted data into different cloud
 environments. The main focus of the tool is to use secure storage in the
@@ -7,10 +7,18 @@ cloud: all data is encrypted and no information about what kind of data is
 stored is revealed to cloud provider.
 
 The security and privacy of the data is based on GPG encryption, which is
-performed outside the cloud environment. You MUST keep your GPG keyring safe,
-otherwise your data is not safe either.
+performed outside the cloud environment.
 
-Currently Amazon S3 clouds and SFTP filesystems are supported.
+**YOU MUST KEEP YOUR GPG KEYRING SAFE, OTHERWISE YOUR DATA IS NOT SAFE
+EITHER.**
+
+Currently the following storage systems are supported:
+
+* Amazon S3
+* SFTP filesystems
+
+.. todo:: Add support for Google Cloud Storage.
+.. todo:: Add support for Dropbox.
 
 Requirements
 ============
@@ -55,10 +63,10 @@ All code **MUST** have unit tests.
 Functionality
 -------------
 
-The data must be identified only by the hash of the original data. The
-information about the available files (metadata) must be stored separately
-in cloud and locally (local database). This metadata must also be encrypted
-in cloud so that no information is revealed to cloud provider.
+The data **MUST** be identified only by the hash of the original data. The
+information about the available files (metadata) **MUST** be stored
+separately in cloud and locally (local database). This metadata **MUST** also
+be encrypted in cloud so that no information is revealed to cloud provider.
 
 Performance
 -----------
@@ -86,29 +94,43 @@ data are encrypted and signed using GPG:
 
    digraph G {
 
-     node [shape="box"];
-
      subgraph cluster_host {
-       "file" [label="File"];
-       "metadata" [label="Create\\nmetadata"];
-       "gpg" [label="Use GPG to\\nencrypt and\\nsign data"];
-       "database" [label="Store metadata\\nto local\\ndatabase"];
+       "File" [shape="box"];
+       "Create metadata" [label="Create\\nmetadata", shape="circle"];
+       "Metadata" [shape="box"];
+       "GPG data" [label="Use GPG to\\nencrypt and\\nsign data",
+       shape="circle"];
+       "GPG metadata" [label="Use GPG\\nto encrypt\\nand sign\\nmetadata",
+       shape="circle"];
+       "Store metadata to database" [label="Store\\nmetadata\\nto\\ndatabase",
+       shape="circle"];
+       "Database" [shape="box"];
+       "Encrypted data" [label="Encrypted\\nfile data\\nand\\nchecksums",
+       shape="box"];
+       "Store checksums to metadata" [label="Store\\nchecksums\\nto metadata",
+       shape="circle"];
+       "Encrypted metadata" [label="Encrypted\\nmetadata", shape="box"];
+       "Store data" [label="Store data\\nto cloud", shape="circle"];
+       "Store metadata" [label="Store\\nmetadata\\nto cloud", shape="circle"];
        label="User host";
      }
 
      subgraph cluster_s3 {
-       "data_bucket" [label="Data\\nBucket"];
-       "metadata_bucket" [label="Metadata\\nBucket"];
+       "Data bucket" [shape="box"];
+       "Metadata bucket" [shape="box"];
        label="Amazon S3";
      }
 
-     "file" -> "metadata" [label="file data"];
-     "metadata" -> "database" [label="metadata"];
-     "file" -> "gpg" [label="file data"];
-     "metadata" -> "gpg" [label="metadata"];
-     "gpg" -> "metadata_bucket" [label="encrypted\\nand\\nsigned\\nmetadata"];
-     "gpg" -> "data_bucket" [label="encrypted\\nfile data"];
-
+     "File" -> "Create metadata" -> "Metadata" ->
+       "Store metadata to database" -> "Database";
+     "Metadata" -> "GPG metadata" -> "Encrypted metadata" ->
+     "Store metadata";
+     "Store metadata" -> "Metadata bucket"
+     [label="Checksum\\nof plaintext\\nappended with\\nfile path\\nis the key\\nto bucket"];
+     "File" -> "GPG data" -> "Encrypted data" -> "Store data";
+     "Store data" -> "Data bucket"
+     [label="Checksum\\nof plaintext\\nis the key\\nto bucket"];
+     "Encrypted data" -> "Store checksums to metadata" -> "Metadata";
    }
 
 Use GPG to only encrypt and sign metadata
@@ -120,36 +142,7 @@ The following flow chart describes the mode where only metadata is
 encrypted and signed using GPG. File data is encrypted using symmetric
 **AES-256** encryption:
 
-.. graphviz::
-
-   digraph G {
-
-     node [shape="box"];
-
-     subgraph cluster_host {
-       "file" [label="File"];
-       "metadata" [label="Create\\nmetadata\\nand data\\nencryption\\nkey"];
-       "gpg" [label="Use GPG to\\nencrypt and sign\\nmetadata"];
-       "database" [label="Store metadata\\nto local\\ndatabase"];
-       "encrypt" [label="Encrypt data"];
-       label="User host";
-     }
-
-     subgraph cluster_s3 {
-       "data_bucket" [label="Data\\nBucket"];
-       "metadata_bucket" [label="Metadata\\nBucket"];
-       label="Amazon S3";
-     }
-
-     "file" -> "metadata" [label="file data"];
-     "metadata" -> "database" [label="metadata"];
-     "metadata" -> "gpg" [label="metadata"];
-     "metadata" -> "encrypt" [label="encryption\\nkey"];
-     "gpg" -> "metadata_bucket" [label="encrypted\\nand\\nsigned\\nmetadata"];
-     "file" -> "encrypt" [label="file data"];
-     "encrypt" -> "data_bucket" [label="encrypted\\nfile data"];
-
-   }
+.. todo:: Document this.
 
 Use GPG to only encrypt and sign metadata, data in encrypted in cloud
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -160,57 +153,59 @@ The following flow chart describes the mode where only metadata is
 encrypted and signed using GPG. File data is encrypted using symmetric
 **AES-256** encryption in another cloud node:
 
-.. graphviz::
+.. todo:: Document this.
 
-   digraph G {
+Retrieve data from cloud
+------------------------
 
-     node [shape="box"];
+Use GPG to verify and decrypt both metadata and data
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+The following flow
 
-     subgraph cluster_host {
-       "file" [label="File"];
-       "metadata" [label="Create\\nmetadata\\nand data\\nencryption\\nkey"];
-       "gpg" [label="Use GPG to\\nencrypt and sign\\nmetadata"];
-       "database" [label="Store metadata\\nto local\\ndatabase"];
-       label="User host";
-     }
+The following flow chart describes the mode where both metadata and file
+data are verified and decrypted using GPG. Metadata is stored in
+plaintext in local database:
 
-     subgraph cluster_ec2 {
-       "encrypt" [label="Encrypt data"];
-       label="Amazon EC2";
-     }
+.. todo:: Document this.
 
-     subgraph cluster_s3 {
-       "data_bucket" [label="Data\\nBucket"];
-       "metadata_bucket" [label="Metadata\\nBucket"];
-       label="Amazon S3";
-     }
+Use GPG to only encrypt and sign metadata
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-     "file" -> "metadata" [label="file data"];
-     "metadata" -> "database" [label="metadata"];
-     "metadata" -> "gpg" [label="metadata"];
-     "metadata" -> "encrypt" [label="encryption\\nkey"];
-     "gpg" -> "metadata_bucket" [label="encrypted\\nand\\nsigned\\nmetadata"];
-     "file" -> "encrypt" [label="file data"];
-     "encrypt" -> "data_bucket" [label="encrypted\\nfile data"];
+.. todo:: Data encryption without GPG is not implemented yet.
 
-   }
+The following flow chart describes the mode where only metadata is
+encrypted and signed using GPG. File data is encrypted using symmetric
+**AES-256** encryption:
+
+.. todo:: Document this.
+
+Use GPG to only encrypt and sign metadata, data in encrypted in cloud
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. todo:: Data encryption in cloud is not implemented yet.
+
+The following flow chart describes the mode where only metadata is
+encrypted and signed using GPG. File data is encrypted using symmetric
+**AES-256** encryption in another cloud node:
+
+.. todo:: Document this.
 
 User stories
 ============
 
-The basic functionality of the tool is described as user stories.
+The basic functionality of GPGCloud is described as user stories.
 
 Store one file to cloud
 -----------------------
 
-I, as the user of the tool, want to be able to store one file to cloud so
+I, as the user of GPGCloud, want to be able to store one file to cloud so
 that no information about where the data comes from or what the data is is
 revealed to the cloud provider.
 
 List available files in cloud
 -----------------------------
 
-I, as the user of the tool, want to be able to list all files stored to cloud.
+I, as the user of GPGCloud, want to be able to list all files stored to cloud.
 It must be possible to see the metadata of the stored files, similar to ``ls
 -l`` output:
 
@@ -227,22 +222,22 @@ It must be possible to see the metadata of the stored files, similar to ``ls
 Retrieve one file from the cloud
 --------------------------------
 
-I, as the user of the tool, want to be able to retrieve one file from cloud to
+I, as the user of GPGCloud, want to be able to retrieve one file from cloud to
 local directory, either to original location or to a given location with a
 given file name.
 
 Store directory to cloud
 ------------------------
 
-I, as the user of the tool, want to be able to store one directory to cloud so
-that no information about where the data comes from or what the data is is
+I, as the user of GPGCloud, want to be able to store one directory to cloud
+so that no information about where the data comes from or what the data is is
 revealed to the cloud provider. All files in the given directory are stored
 to cloud.
 
 Retrieve directory from cloud
 -----------------------------
 
-I, as the user of the tool, want to be able to retrieve one directory from
+I, as the user of GPGCloud, want to be able to retrieve one directory from
 cloud to local directory, either to original location or to a given location
 with a given directory name.
 
